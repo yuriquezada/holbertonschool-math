7a7b5247013094b6cd451c919d32636c0dcdde83
yuriquezada <quezadat.yuri@gmail.com>
2021-07-28 15:07:19 -0700

    Create 0-display.c file

---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+/**
+ * struct print - print type and print function
+ * @char_to_compare: print type
+ * @f: print function
+ */
+typedef struct number_complex
+{
+	int re;
+	int im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif
