b471b556f92cb2e76ebb28513903c999ed2569b0
yuriquezada <quezadat.yuri@gmail.com>
2021-07-29 00:01:08 -0700

    Create complex_from_mod_arg function

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -17,5 +17,5 @@ void division(complex c1, complex c2, complex *c3)
 	double q = c2.im;
 
 	c3->re = ((a * p) + (b * q)) / (pow(p, 2) + pow(q, 2));
-	c3->im = ((b * p) + (a * q)) / (pow(p, 2) + pow(q, 2));
+	c3->im = ((b * p) - (a * q)) / (pow(p, 2) + pow(q, 2));
 }
