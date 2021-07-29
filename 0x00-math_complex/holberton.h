79a0473eddc89f74cf21094945edb4f0b2b4cd29
yuriquezada <quezadat.yuri@gmail.com>
2021-07-28 21:35:08 -0700

    Create substraction function

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -17,5 +17,7 @@ void display_complex_number(complex c);
 complex conjugate(complex c);
 double modulus(complex c);
 double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
+void substraction(complex c1, complex c2, complex *c3);
 
 #endif
