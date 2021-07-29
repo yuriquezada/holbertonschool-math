f7aed6a32dcd6ac60ae65a03b2c4d2eb7e6aeb79
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-07-29 01:11:32 -0500

    Change %d for %f

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -11,23 +11,23 @@ void display_complex_number(complex c)
 	if (c.re == 0)
 	{
 		if (c.im < 0)
-			printf("- %di\n", c.im * (-1));
+			printf("- %fi\n", c.im * (-1));
 		else if (c.im == 0)
 			printf("0\n");
 		else
-			printf("%d\n", c.im);
+			printf("%f\n", c.im);
 	}
 	else
 	{
 		if (c.im < -1)
-			printf("%d - %di\n", c.re, c.im * (-1));
+			printf("%f - %fi\n", c.re, c.im * (-1));
 		else if (c.im == -1)
-			printf("%d - i\n", c.re);
+			printf("%f - i\n", c.re);
 		else if (c.im == 0)
-			printf("%d\n", c.re);
+			printf("%f\n", c.re);
 		else if (c.im == 1)
-			printf("%d + i\n", c.re);
+			printf("%f + i\n", c.re);
 		else
-			printf("%d + %di\n", c.re, c.im);
+			printf("%f + %fi\n", c.re, c.im);
 	}
 }
